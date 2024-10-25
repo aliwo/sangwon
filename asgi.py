0f4c589b4a6c72679fa8c5b6f4afe7a0496e5fa9
SW <aliwo@naver.com>
2024-10-25 19:00:45 +0900

    add health router

--- asgi.py
+++ asgi.py
@@ -4,3 +4,6 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+"gunicorn asgi:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001"
